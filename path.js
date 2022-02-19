9273a600118d0e8561b8bc9731e087e84f33edd7
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2022-02-19 11:14:02 +0600

    path module

---
+++ path.js
@@ -0,0 +1,13 @@
+import path from "path";
+import { log } from "./functions.js";
+const indexPath = "E:/Learning_Backend/Node.js/Day_1/index.js";
+const newPath = "E:/Learning_Backend";
+
+console.log(log("dirName"), path.dirname(indexPath));
+console.log(log("baseName"), path.basename(indexPath));
+console.log(log("extName"), path.extname(indexPath));
+console.log(log("isAbsolute"), path.isAbsolute(indexPath));
+console.log(log("join"), path.join("foo/", "bar/"));
+console.log(log("parse"), path.parse(indexPath));
+console.log(log("relative"),path.relative(indexPath, newPath));
+console.log(log("separetor"),path.sep);
