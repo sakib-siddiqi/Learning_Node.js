1f8250c9ade4fb3e2f6fbaadf3585ecf53baa971
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2022-02-20 12:13:07 +0600

    Event Modules

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 import { arrow, rocket } from "./icon.js";
 
 let sumOfArr = (arr = []) => arr.reduce((a, b) => a + b, 0);
-let log=(log)=> `${rocket} ${log} ${arrow}` ;
-export {sumOfArr,log}
+let log = (log) => `${rocket} ${log} ${arrow}`;
+let print = (data) =>
+  console.log(`🚀 ${new Date().toLocaleTimeString()} : ${data}`);
+export { sumOfArr, log,print };
